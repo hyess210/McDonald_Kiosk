127a9c4355f84176363bb5b2c4277f5e88d724f0
SungHoon <leesunghoon03@naver.com>
2020-10-06 11:59:19 +0900

    [ADD] Basic Design
    [ADD] - Basic Design

--- SelectPayment.xaml.cs
+++ SelectPayment.xaml.cs
@@ -24,5 +24,10 @@ namespace McDonald_Kiosk
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
