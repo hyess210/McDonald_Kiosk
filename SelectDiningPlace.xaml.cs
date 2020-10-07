822d9c588ae3d42e020b4440372198c36762c23f
Joohyun Bae <baejoohyunn@daum.net>
2020-10-07 15:52:50 +0900

    [Add, Update] DiningPlace Frames 생성

---
+++ SelectDiningPlace.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace McDonald_Kiosk
+{
+    /// <summary>
+    /// SelectDiningPlace.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SelectDiningPlace : Page
+    {
+        public SelectDiningPlace()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SelectDiningTable table = new SelectDiningTable();
+            NavigationService.Navigate(table);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
