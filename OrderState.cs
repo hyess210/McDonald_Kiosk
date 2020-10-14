fe0ffbc313a305bc5f937db2fe8c1d650dd0bb93
SungHoon <leesunghoon03@naver.com>
2020-10-14 16:09:40 +0900

    [ADD] OrderState.cs
    [ADD] - OrderState.cs

--- OrderState.cs
+++ OrderState.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace McDonald_Kiosk
 {
-    class OrderState
+    public class OrderState
     {
-        public string Menu;
-        public int Price;
-        public int Amount;
-        public int Total;
+        public string Menu { get; set; }
+        public int Price { get; set; }
+        public int Amount { get; set; }
+        public int Total { get; set; }
 
-        public static List<OrderState> orderState;
+        private static List<OrderState> orderState;
+
+        public static List<OrderState> GetState()
+        {
+            if (orderState == null)
+                orderState = new List<OrderState>();
+
+            return orderState;
+        }
     }
 }
