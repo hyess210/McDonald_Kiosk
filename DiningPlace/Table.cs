6e6f33529386c4425f316c4a1fd5f4ca1c12f060
Joohyun Bae <baejoohyunn@daum.net>
2020-11-04 16:09:24 +0900

    [UPDATE] 업데이트
    업데이트 (미안해요 쓰기 너무 귀찮아)

--- DiningPlace/Table.cs
+++ DiningPlace/Table.cs
@@ -9,7 +9,7 @@ namespace McDonald_Kiosk
 {
     class Table
     {
-        public int left_time = 60;
+        public int left_time = 0;
         public bool isEnabled = false;
         public DispatcherTimer timer;
     }
