306ef094c2d21026487c66805e4fc3bb187118cd
SungHoon <leesunghoon03@naver.com>
2020-11-10 11:12:42 +0900

    [ADD] Customer_Singleton Class
    [ADD] - 사용자 정보를 담는 Customer클래스 설정

---
+++ Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McDonald_Kiosk
+{
+    public class Customer
+    {
+        public int order_idx;
+        public int user_idx;
+        public DateTime order_time;
+        public int tableNum;
+
+        private static Customer customer = new Customer();
+
+        public Customer getInstance()
+        {
+            if (customer == null)
+                customer = new Customer();
+
+            return customer;
+        }
+    }
+}
