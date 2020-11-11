d1887196ae9d8978595cd4f638a7a08ded7cdfed
SungHoon <leesunghoon03@naver.com>
2020-11-11 13:58:40 +0900

    [ADD] OrderNumber_Select
    [ADD]
    -주문번호 받아오기

--- OrderNumber.xaml.cs
+++ OrderNumber.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,6 +30,29 @@ namespace McDonald_Kiosk
 
         public void Label_Loaded(object sender, RoutedEventArgs e)
         {
+            string connStr = "Server=localhost;Database=mcdonald_kiosk;Uid=root;Pwd=kmk5632980;";
+            int orderNum = 0;
+
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                string sql = "SELECT * FROM ordering";
+
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    orderNum = int.Parse(rdr["order_idx"].ToString());
+                }
+                rdr.Close();
+            }
+
+            Customer.getInstance().order_idx = orderNum + 1;
+
+            Label label = (Label)sender;
+            label.Content = "주문번호 : " + Customer.getInstance().order_idx;
+
             timer.Interval = TimeSpan.FromSeconds(15);
             timer.Tick += GoHomePage;
             timer.Start();
